2dd202e4410c02fdbd170b3937f01f7aa81bf28a
Nathan Pickard <nathan.pickard87@gmail.com>
2018-12-06 21:28:49 -0800

    Updated git search service to rxjs and Observables

--- src/app/git-search.service.ts
+++ src/app/git-search.service.ts
@@ -1,29 +1,49 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-// import 'rxjs/add/operator/toPromise';
+
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/publishReplay';
 
 import { GitSearch } from './git-search';
+import { GitUsers } from './git-users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitSearchService {
-  cachedValues: Array<{
-    [query: string]: GitSearch
+  cachedSearches: string;
+  search: Observable<GitSearch>;
+  cachedUsers: Array<{
+    [query: string]: GitUsers
   }> = [];
 
   constructor(private http: HttpClient) { }
 
-  gitSearch = (query: string): Promise<GitSearch> => {
-    let promise = new Promise<GitSearch>((resolve, reject) => {
-      if (this.cachedValues[query]) {
-        resolve(this.cachedValues[query])
+  gitSearch: Function = (query: string): Observable<GitSearch> => {
+    if (!this.search) {
+      this.search = this.http.get<GitSearch>('https://api.github.com/search/repositories?q=' + query)
+        .publishReplay(1)
+        .refCount();
+        this.cachedSearches = query;
+    }
+    else if (this.cachedSearches !== query) {
+      this.search = null;
+      this.gitSearch(query);
+    }
+    return this.search;
+  }
+
+  gitUsers = (query: string): Promise<GitUsers> => {
+    let promise = new Promise<GitUsers>((resolve, reject) => {
+      if (this.cachedUsers[query]) {
+        resolve(this.cachedUsers[query])
       }
       else {
-        this.http.get('https://api.github.com/search/repositories?q=' + query)
+        this.http.get('https://api.github.com/search/users?q=' + query)
           .toPromise()
           .then((response) => {
-            resolve(response as GitSearch)
+            resolve(response as GitUsers)
           }, (error) => {
             reject(error);
           })
